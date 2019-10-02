b766a03b62650664b1f57950797473c237674fca
Dale LaPlante <bmbmjmdm@gmail.com>
2019-10-01 21:41:03 -0600

    Reduce image sizes

--- wesleyapp/components/fwbpList.js
+++ wesleyapp/components/fwbpList.js
@@ -1,7 +1,7 @@
-import dogPic from '../assets/dog.png';
+import dogPic from '../assets/dog.jpg';
 import funPic from '../assets/fun.jpg';
 import readingPic from '../assets/reading.jpg';
-import applePic from '../assets/apple.png';
+import applePic from '../assets/apple.jpg';
 
 export default [
     {targetWord: "dog", allWords: [{word: "fun", pic: funPic}, {word: "reading", pic: readingPic}, {word: "apple", pic: applePic}, {word: "dog", pic: dogPic}] },
--- wesleyapp/components/fwisList.js
+++ wesleyapp/components/fwisList.js
@@ -1,7 +1,7 @@
-import dogPic from '../assets/dog.png';
+import dogPic from '../assets/dog.jpg';
 import funPic from '../assets/fun.jpg';
 import readingPic from '../assets/reading.jpg';
-import applePic from '../assets/apple.png';
+import applePic from '../assets/apple.jpg';
 
 export default [
     {sentence: "The dog is my friend", targetWord: "dog", pic: dogPic},
