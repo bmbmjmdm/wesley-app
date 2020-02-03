e5040ae46c73ca7840cc3a07ce46887ac3d24ded
Dale LaPlante <bmbmjmdm@gmail.com>
2020-02-02 22:02:03 -0700

    Upgraded RN, needs asset fixing and ios upgrade

--- wesleyapp/index.js
+++ wesleyapp/index.js
@@ -1,7 +1,9 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import 'react-native-audio'
-import 'react-native-sound'
-import 'react-native-tts'
+/**
+ * @format
+ */
 
-AppRegistry.registerComponent('wesleyapp', () => App);
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
--- wesleyapp/metro.config.js
+++ wesleyapp/metro.config.js
@@ -5,16 +5,24 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    sourceExts: ['jsx', 'js', 'ts'],
-  },
-};
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("./vueTransformerPlugin.js"),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      })
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "vue"]
+    }
+  };
+})();
